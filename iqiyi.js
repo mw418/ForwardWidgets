5cc7a927d8a378d991cebde897e6b65c0518680c
mw99 <tqm0601@qq.com>
2025-04-10 21:20:45 +0800

    使用tmdb id

--- iqiyi.js
+++ iqiyi.js
@@ -57,10 +57,20 @@ var WidgetMetadata = {
     const videoIds = [];
     let data = response.data["items"][0]["video"][0]
     for (const item of data.data) {
+        let title = item.display_name
+        const res = await Widget.http.get(url, {
+            headers: {
+              Referer: "https://www.themoviedb.org/search/trending?query=" + encodeURIComponent(title),
+              "User-Agent":
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                "Host": "www.themoviedb.org",
+            },
+          });
+        let id = res.data.results[0].id
         videoIds.push({
-            id: item.play_url,
-            type: "url",
-            title: item.display_name,
+            id: id,
+            type: "tmdb",
+            title: title,
             description: item.description,
             coverUrl: item.image_cover,
           });
